efa437a2d7d4ff4902a5f2d0cbbb30831374035b
charlie.brown <cdb.binary@gmail.com>
2017-11-25 04:03:31 +0000

    removed tab formatting and used spaces for secrets.yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,20 +26,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # learn-rails
   config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
@@ -48,12 +34,23 @@ Rails.application.configure do
   user_name: Rails.application.secrets.email_provider_username,
   password: Rails.application.secrets.email_provider_password
   }
-
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
